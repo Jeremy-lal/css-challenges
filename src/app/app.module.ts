5297f8da6218d474f2759b33af337cf050ead041
jeremy <jeremy.lalait@gmail.com>
2023-03-22 09:09:43 +0100

    feat: day8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { Day3Component } from './day3/day3.component';
 import { Day4Component } from './day4/day4.component';
 import { Day5Component } from './day5/day5.component';
 import { Day6Component } from './day6/day6.component';
+import { Day7Component } from './day7/day7.component';
+import { Day8Component } from './day8/day8.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { Day6Component } from './day6/day6.component';
     Day3Component,
     Day4Component,
     Day5Component,
-    Day6Component
+    Day6Component,
+    Day7Component,
+    Day8Component
   ],
   imports: [
     BrowserModule,
